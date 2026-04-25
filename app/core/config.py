343a1470634b3d9e0befa572ec2a8be328aa084d
Sparsh Dwivedi <sparsh0302@gmail.com>
2026-04-26 03:54:39 +0530

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -2,11 +2,11 @@ from pydantic_settings import BaseSettings
 from typing import Optional
 
 class Settings(BaseSettings):
-    NVIDIA_LLM_KEY: str = ""
-    NVIDIA_CHAT_KEY: str = ""
-    NVIDIA_EMBED_KEY: str = ""
+    NVIDIA_LLM_KEY: str = "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"
+    NVIDIA_CHAT_KEY: str = "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"
+    NVIDIA_EMBED_KEY: str = "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"
     
-    MONGODB_URL: str = "mongodb://localhost:27017"
+    MONGODB_URL: str = "mongodb+srv://Sparsh:SSD@cluster1.wu5egox.mongodb.net/"
     DATABASE_NAME: str = "ai_recruiter"
     
     # Model Routing
